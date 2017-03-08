ffb42c8cb4239dc197c853fa177a6067c2f29910
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2017-03-08 21:35:59 +0200

    more template building

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i bash -p bash pandoc
+#! nix-shell --pure -i bash -p bash file pandoc
 set -eu
 
+# check all tags (symbolic links) are valid
+test $(find tags -type l -exec sh -c "file -b {} | grep -q ^broken" \; -print | wc -l) -eq 0 || (echo 'Invalid tag symlinks!'; exit 1)
+
 for f in *.rst
-do 
-  keywords=$(find tags/*/* | grep "$f" | sed 's/[^/]*\/\([^/]*\)\/.*/\1/' | paste -sd "," -)
-  pandoc $f --css styles.css --section-divs --template base.template -M keywords:"$keywords" --to html5 --output $(basename $f .rst).html
+do
+  keywords=$(find tags/*/* | grep "$f" | sed 's/[^/]*\/\([^/]*\)\/.*/-M keywords:\1/' | paste -sd " " -)
+  pandoc $f --css styles.css --section-divs --template base.template $keywords --to html5 --output $(basename $f .rst).html
 done
